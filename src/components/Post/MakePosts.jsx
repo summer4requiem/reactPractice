04d502e676c9f766c8bb70350064a5b68d5d70c7
summer4requiem <summer.requiem96@gmail.com>
2020-09-23 13:33:40 +0300

    axios, get users from server

--- src/components/Post/MakePosts.jsx
+++ src/components/Post/MakePosts.jsx
@@ -19,7 +19,7 @@ const MakePosts = (props) => {
       <div className={s.posts}>
         <label className={s.postsHeader} htmlFor="post">My posts</label>
         <div className={s.postsWrapper}>
-          <textarea onChange={onPostChange} name="post" id="post" cols="30" rows="10" width="100%" value={props.newPostText} />
+          <textarea placeholder="What's happening ?" onChange={onPostChange} name="post" id="post" cols="30" rows="10" width="100%" value={props.newPostText} />
           <button onClick={onPostsSendBtn} className={s.postsSendBtn} type="submit">post</button>
         </div>
       </div>
